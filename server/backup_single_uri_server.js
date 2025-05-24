c355d9f2168f58acd16fb428f292c043e5714bf2
Vidun Hettiarachchi <vidun_hettiarachchi@Viduns-MacBook-Air.local>
2025-05-24 18:34:07 +0530

    Fixed server.js

---
+++ server/backup_single_uri_server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+const internRoutes = require('./routes/internRoutes');
+app.use('/api/interns', internRoutes);
+
+const attendanceRoutes = require('./routes/attendanceRoutes');
+app.use('/api/attendance', attendanceRoutes);
+
+const salaryRoutes = require('./routes/salaryRoutes');
+app.use('/api/salaries', salaryRoutes);
+
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(5000, () => console.log('Server running on port 5000'));
+    })
+    .catch(err => console.log(err));
